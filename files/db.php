8da98dd328ca53903cd593b6bd2eb1f114c16d28
Steven Stromberg <gyozaguy@gmail.com>
2012-10-11 23:32:07 -0600

    Removed database information for security reasons

--- files/db.php
+++ files/db.php
@@ -1,9 +1,9 @@
 <?php
 	session_start();
 	$sv = "localhost";
-	$un = "gyozaguy_chnghsn";
-	$pw = "simple12";
-	$db = "gyozaguy_chunghsin";
+	$un = "<username>";
+	$pw = "<password>";
+	$db = "<database>";
 	$con = new mysqli($sv, $un, $pw, $db);
 	if (mysqli_connect_error()) {
 		header('Location: ../?e=500');
